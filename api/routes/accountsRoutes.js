c2438714e515aa77d78cb6d1e6405b3d5001ae12
raythurman2386 <raymondthurman5@gmail.com>
2019-12-17 22:19:35 -0500

    minor updates to files

--- api/routes/accountsRoutes.js
+++ api/routes/accountsRoutes.js
@@ -1,44 +1,52 @@
 const accountsRouter = require('express').Router();
 const db = require('../../data/dbConfig');
 
-accountsRouter
-  .get('/', async (req, res, next) => {
-    try {
-      const accounts = db('accounts')
-      res.status(200).json(accounts)
-    } catch (error) {
-      next(error)
-    }
-  })
-  .get('/:id', async (req, res, next) => {
-    try {
 
-    } catch (error) {
-      next(error)
-    }
-  })
-  .post('/', async (req, res, next) => {
-    try {
-
-    } catch (error) {
-      next(error)
-    }
-  })
-  .put('/:id', async (req, res, next) => {
-    try {
-
-    } catch (error) {
-      next(error)
-    }
-  })
-  .delete('/:id', async (req, res, next) => {
-    try {
-
-    } catch (error) {
-      next(error)
-    }
-  })
-
-module.exports = server => {
-  server.use('/api/accounts', accountsRouter);
+const getAccounts = async (req, res, next) => {
+  try {
+    const accounts = await db('accounts')
+    return res.status(200).json(accounts)
+  } catch (error) {
+    next(error)
+  }
 }
+
+const getAccountById = async (req, res, next) => {
+  try {
+    const [account] = await db('accounts').where({ id: req.params.id })
+    return res.status(200).json(account)
+  } catch (error) {
+    next(error)
+  }
+}
+const addAccount = async (req, res, next) => {
+  try {
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+const updateAccount = async (req, res, next) => {
+  try {
+
+  } catch (error) {
+    next(error)
+  }
+}
+const deleteAccount = async (req, res, next) => {
+  try {
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+accountsRouter
+  .get('/', getAccounts)
+  .get('/:id', getAccountById)
+  .post('/', addAccount)
+  .put('/:id', updateAccount)
+  .delete('/:id', deleteAccount);
+
+module.exports = accountsRouter;
--- api/server.js
+++ api/server.js
@@ -1,15 +1,10 @@
 const express = require('express');
+const accountsRouter = require('./routes/accountsRoutes');
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', accountsRouter)
 
-server.use('/', (req, res) => {
-  res.status(200).json({ message: "API is Working" })
-})
-
-server.use((err, req, res, next) => {
-  res.status(500).json({ message: "Something went Wrong" })
-})
 
 module.exports = server;
